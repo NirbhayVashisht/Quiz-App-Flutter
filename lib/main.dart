634b30b7ab72da613235ee2fe3b0e3215ee92e78
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-03-17 14:08:50 +0530

    Added if-else block(basic app completed)

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite color ?',
+      'answers': ['red', 'yellow', 'blue', 'Other'],
+    },
+    {
+      'questionText': 'What\'s your favourite food ?',
+      'answers': ['Indian', 'Italian', 'Chinese', 'Other'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal ?',
+      'answers': ['Dog', 'Cat', 'Lion', 'Other'],
+    },
+  ];
+
   void _answerQuestion() {
     setState(() {
       _questionIndex++;
@@ -24,40 +39,30 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favourite color ?',
-        'answers': ['red', 'yellow', 'blue', 'Other'],
-      },
-      {
-        'questionText': 'What\'s your favourite food ?',
-        'answers': ['Indian', 'Italian', 'Chinese', 'Other'],
-      },
-      {
-        'questionText': 'What\'s your favourite animal ?',
-        'answers': ['Dog', 'Cat', 'Lion', 'Other'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText']),
-            
-            //below we are transforming the list into list of widgets
-            //map method executes a function which we have to pass as an argument
-            //so below we are passing an anonymous function
-            //... are spread operator
-            //all this is done in lec 33 Mapping List to Widgets
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
-              return Answer(_answerQuestion,answer);     
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText']),
+
+                  //below we are transforming the list into list of widgets
+                  //map method executes a function which we have to pass as an argument
+                  //so below we are passing an anonymous function
+                  //... are spread operator
+                  //all this is done in lec 33 Mapping List to Widgets
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it'),
+              ),
       ),
     );
   }
